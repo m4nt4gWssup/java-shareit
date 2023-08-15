bfc194fa6dcb791690363bce02cc2a784566f3cb
Петруха <petro.mandazhi@yandex.ru>
2023-08-15 06:06:46 +0300

    refactor: доработан код

--- server/src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import ru.practicum.shareit.checker.Checker;
 import ru.practicum.shareit.exception.EntityNotFoundException;
 import ru.practicum.shareit.exception.ItemNotFoundException;
